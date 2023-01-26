aedf5299f0f1229ccecc3a8b893a296f206cbed2
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-26 16:17:58 +0100

    Creating the first React component.

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+function App()
+{
+    return (
+        <section>
+            <h1>Hello world !</h1>
+            <p>This is my first app without CRA ! :D</p>
+        </section>
+    )
+}
+
+export default App;
