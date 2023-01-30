a023068b85b26088f9f29e0a30f7f893f871ee6d
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-30 15:07:36 +0100

    Creation of the page to display the list of existing employees.

---
+++ src/pages/EmployeeList/index.jsx
@@ -0,0 +1,13 @@
+//React
+import React from 'react';
+
+function EmployeeList()
+{  
+  return (
+    <React.Fragment>
+        <h1>Current Employees</h1>
+    </React.Fragment>
+  );
+}
+
+export default EmployeeList;
