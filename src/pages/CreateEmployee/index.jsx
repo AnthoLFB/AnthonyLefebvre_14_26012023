615899fce762c5139b6307ae9853109b3600773c
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-01-30 15:06:43 +0100

    Creation of the page for creating employees.

---
+++ src/pages/CreateEmployee/index.jsx
@@ -0,0 +1,13 @@
+//React
+import React from 'react';
+
+function CreateEmployee()
+{  
+  return (
+    <React.Fragment>
+        <h1>Create Employee</h1>
+    </React.Fragment>
+  );
+}
+
+export default CreateEmployee;
