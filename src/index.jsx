d7dbfc21b36f9ece8487925643d29aa78ca0fd1e
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-05 21:32:39 +0100

    Add the store provider

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,15 @@ import Router from './router/Router';
 import './styles/global/reset.css';
 import './styles/global/font.css';
 
+//Redux
+import { Provider } from 'react-redux';
+import store from './utils/store';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </Provider>
 );
