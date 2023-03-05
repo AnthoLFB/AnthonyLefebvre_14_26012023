13f31900a72be3c4477ff108632c3d74dd633419
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-05 17:13:35 +0100

    import the form

--- src/pages/CreateEmployee/index.jsx
+++ src/pages/CreateEmployee/index.jsx
@@ -1,11 +1,13 @@
 //React
 import React from 'react';
+import CreateEmployeeForm from '../../components/CreateEmployeeForm';
 
 function CreateEmployee()
-{  
+{
   return (
     <React.Fragment>
         <h1>Create Employee</h1>
+        <CreateEmployeeForm />
     </React.Fragment>
   );
 }
