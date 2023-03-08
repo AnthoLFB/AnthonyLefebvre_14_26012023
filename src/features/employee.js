648a65019fd9581e03b413aaea93184d79165da7
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-08 15:17:17 +0100

    Remove useless modal controls. They are now on the employeeForm component.

--- src/features/employee.js
+++ src/features/employee.js
@@ -1,4 +1,3 @@
-
 //Data
 import employees from '../assets/data/employees.json';
 
@@ -11,22 +10,15 @@ const initialState = {
 const ADD_EMPLOYEE_TO_LIST = "addEmployeeToList";
 
 //Action Creator
-export function createNewEmployee(store, newEmployee, modalControls)
+export function createNewEmployee(store, newEmployee)
 {   
     try 
     {
         store.dispatch({type: ADD_EMPLOYEE_TO_LIST, payload: newEmployee});
-        modalControls.title("Employee created !")
-        modalControls.message( newEmployee.firstName + ' ' + newEmployee.lastName + " has been successfully added to the list of employees.")
-        modalControls.type("success")
-        modalControls.status(true)
     } 
     catch (error) 
     {
-        modalControls.title("Error !")
-        modalControls.message(error)
-        modalControls.type("error")
-        modalControls.status(true)
+        console.log(error)
     }
 }
 
