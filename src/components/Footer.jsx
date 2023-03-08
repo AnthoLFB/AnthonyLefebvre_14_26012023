6400e5e98ed6485fdf5973956b1f624aebf969a6
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-08 15:16:08 +0100

    Create the footer component.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+//CSS
+import '../styles/components/Footer.css';
+
+function Footer()
+{
+    return (
+        <footer className="footer">
+            <p className="footer__content">HRnet by WealthHealth - 2023</p>
+        </footer>
+    )
+}
+
+export default Footer
