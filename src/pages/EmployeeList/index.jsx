5fcc1d83f5b844176cc1c7cfe9bbf4cb7c6902d8
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-08 15:17:54 +0100

    Add table component

--- src/pages/EmployeeList/index.jsx
+++ src/pages/EmployeeList/index.jsx
@@ -1,11 +1,21 @@
 //React
 import React from 'react';
+import EmployeesDataTable from '../../components/Table/EmployeesDataTable';
+
+
+//Redux
+import { useSelector } from 'react-redux';
+import { employeeList } from '../../utils/selectors';
 
 function EmployeeList()
 {  
+
+  const employees = useSelector(employeeList);
+
   return (
     <React.Fragment>
-        <h1>Current Employees</h1>
+        <h1 className='page-title'>Current Employees</h1>
+        <EmployeesDataTable employees={employees}/>
     </React.Fragment>
   );
 }
