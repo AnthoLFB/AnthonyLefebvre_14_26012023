8ef539b331fd42e6d291f8bdfbc2e06f71955440
Anthony LEFEBVRE <antho.lefebvre@outlook.fr>
2023-03-10 15:57:30 +0100

    Add some css rules

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -1,16 +1,17 @@
 //React
 import React from 'react';
 
+//CSS
+import '../../styles/pages/Error.css'
+
 function Error({code, message})
 {
     return (
-        <React.Fragment>
-            <section>
-                <h1>Oops</h1>
-                <p>It seems that an error has occurred...</p>
-                <p>{code} - {message}</p>
-            </section>
-        </React.Fragment>
+        <section className='error'>
+            <h1 className='error__title'>Oops</h1>
+            <p className='error__catchphrase'>It seems that an error has occurred...</p>
+            <p className='error__information'>{code} - {message}</p>
+        </section>
     );
 }
 
